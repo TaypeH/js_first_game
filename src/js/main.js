54f234dea27a6f9bfa82fd15ea947d7e618e272d
denis rotaru <taypeh@gmail.com>
2024-05-19 19:27:52 +0300

    3. Object Oriented Programming

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,41 @@ window.addEventListener('load', function () { // when the page loads
     canvas.height = 500;
     
     const ctx = canvas.getContext('2d'); // get the 2d context of the canvas
+
+    class InputHandler { // class to handle input
+
+    }
+
+    class Projectile { // class to handle projectiles - lasers, bullets, etc.
+
+    }
+
+    class Particle { // class to handle particle effects - falling screws, corks and bolts that come from damaged enemies
+
+    }
+
+    class Player { // class to handle the main character
+
+    }
+    
+    class Enemy { // class to handle enemies
+
+    }
+
+    class Layer { // class to handle layers - background, foreground, etc.
+
+    }
+
+    class Background { // class to handle the background - put all together
+
+    }
+
+    class UI { // class to handle the score, timer and other UI elements
+
+    }
+
+    class Game { // class to handle the game - put all together
+
+    }
+
 });
