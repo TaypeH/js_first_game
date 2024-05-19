7e734ecdce94751223432bd04740a688ce8fa5fe
denis rotaru <taypeh@gmail.com>
2024-05-19 19:45:31 +0300

    4.Creating Player & Game Objects

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () { // when the page loads
     const canvas = document.getElementById('game-canvas');
     canvas.width = 1500;
     canvas.height = 500;
-    
+
     const ctx = canvas.getContext('2d'); // get the 2d context of the canvas
 
     class InputHandler { // class to handle input
@@ -18,9 +18,22 @@ window.addEventListener('load', function () { // when the page loads
     }
 
     class Player { // class to handle the main character
-
+        constructor(game) {
+            this.game = game;
+            this.width = 120; // the same as the sprite
+            this.height = 50; // the same as the sprite
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update() {
+            this.y += this.speedY;
+        }
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
-    
+
     class Enemy { // class to handle enemies
 
     }
@@ -38,7 +51,19 @@ window.addEventListener('load', function () { // when the page loads
     }
 
     class Game { // class to handle the game - put all together
-
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+        update() {
+            this.player.update();
+        }
+        draw(context) {
+            this.player.draw(context);
+        }
     }
+
+    const game = new Game(canvas.width, canvas.height); // create a new game instance
 
 });
